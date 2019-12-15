9b1444577b66c8f8359e7aceb5d5d5c1f5482696
GSCrawley <gscrawley@gmail.com>
2019-12-15 13:54:57 -0800

    get_infected

---
+++ Person.py
@@ -0,0 +1,25 @@
+import random
+from Virus import Virus
+
+class Person(object):
+    ''' The simulation will contain people who will make up a population.'''
+
+    def __init__(self, is_vaccinated, infection=None):
+       
+        self.is_alive = True #boolean
+        self.is_vaccinated = is_vaccinated #boolean
+        self.infection = infection #virus object
+        
+    def did_survive_infection(self):
+        mortality_rate = self.infection.mortality_rate
+        if self.infection:
+            random_float = random.randrange(0,1)
+            if random_float < mortality_rate:
+                self.is_alive = False
+                return False
+            else:
+                self.is_vaccinated =  True
+                self.infection = None
+                return True
+
+        
